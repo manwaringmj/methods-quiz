d68366869842c6037b6f57df018edf487003b3f2
Michael Jacob Manwaring <manwaringmj@dcsdk12.org>
2016-09-08 13:58:36 -0600

    has_teen is finished

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,5 +1,12 @@
-	
+
 # TODO - write has_teen?
+def has_teen?(a,b,c)
+	if a||b||c > 12 && a||b||c < 20
+		return true
+	elsif a||b||c < 13 && a||b||c > 19
+		return false
+	end
+end
 
 # TODO - write not_string
 
